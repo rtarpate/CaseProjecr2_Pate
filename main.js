c2e2e7460563161e7d51ecb3d2088aee8bbf2bb1
rtarpate <arpate@amarillocollege.com>
2025-04-13 03:56:17 -0700

    added validation for user input
    makes sure first naem and last name are strings and email can not be gamil.com

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //selects the audio 
-const audio = document.getElementbyId('backgroundAudio');
+const audio = document.getElementById('backgroundAudio');
 
 //checks to see if the audio stops playing and restarts thge song if it does
 audio.addEventListener('ended', function()
@@ -13,7 +13,57 @@ audio.addEventListener('ended', function()
 
 //adds alert code and listner for contact page
 const contactForm = document.getElementById("contactForm");
+
+//add error handling to the user form
+if(contactForm)
+{
+    contactForm.addEventListener("submit", function(event)
+{   //prevents blank subtions
+    event.preventDefault();
+
+        //gets form data and sets it qeual to testing variables
+        const formData = new FormData(contactForm);
+        const firstName = formData.get("firstName");
+        const lastName = formData.get("lastName");
+        const email = formData.get("myEmail");
+
+        //checks to see if userinput has gamil.com 
+        const blockedDomain = "gmail.com";
+        if(email.toLowerCase().includes(blockedDomain.toLowerCase()))
+        {
+            alert("email gmail.com is not accepted")
+            return;
+        }
+
+        //checks if input for firstname is valid
+        if(!/^[a-zA-Z]+$/.test(firstName))
+        {
+            alert("invalid first name entry");
+            return;
+        }
+        //checks if input for lastname is valid
+        if(!/^[a-zA-Z]+$/.test(lastName))
+        {
+           alert("invalid lasr name entry")
+           return;
+        }
+
+        //alret if succeces
+       // console.log("form data ", formData);
+        alert("Successful submit");
+  
+        //alret if error
+        //console.error("error", error)
+        alert("error submited");
+
+})
+}
+
+
+//comments out redunednt code
+/*
 contactForm.addEventListener("submit", function(event) 
 {
     alert("message sent")
 })
+*/
